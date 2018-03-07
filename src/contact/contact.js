745adc6e7540a8581e778166c24d29f8ea4bd154
Justin <justin.delacruz@gmail.com>
2018-03-07 02:05:43 -0800

    Make contact fields required

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -11,11 +11,11 @@ const Contact = () => (
       <div className="content-block">
         <div className="content-block__body contact__body">
           <form method="POST" action="https://formspree.io/tom@tomraffin.com">
-            <input type="email" name="_replyto" placeholder="Your email" />
+            <input type="email" required name="_replyto" placeholder="Your email" />
             <input type="hidden" name="_next" value="//www.tomraffin.com/thanks" />
             <input type="hidden" name="_subject" value="A message from tomraffin.com" />
             <input type="text" name="_gotcha" style={{ display: 'none' }} />
-            <textarea name="message" placeholder="Your message" rows={5} />
+            <textarea required name="message" placeholder="Your message" rows={5} />
             <input type="submit" value="Send Message to Tom" />
           </form>
         </div>
