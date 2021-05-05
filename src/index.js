f494a373824c4612f1706f66d731b4235b75968d
justindelacruz <jdelacruz@nerdwallet.com>
2021-05-05 08:40:44 -0700

    Disable service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import './fonts.scss';
 import App from './app';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Disable service worker because caching is complicated
+// registerServiceWorker();
